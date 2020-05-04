9b3b2ea8dfb66431613b648057362a3f0e409eb9
Perrine98 <perrine.truche@free.fr>
2020-05-04 11:47:49 +0200

    Rajout d'un auteur

--- G3/graphique regions.R
+++
@@ -1,5 +0,0 @@
-#Graphique ligne 171
-
-res <- PCA(datatemp, quanti.sup=13:16, quali.sup=17,graph=F)
-par(mfrow=c(1,1),mar=c(4,4,2,2))
-barplot(res$eig[,2],ylab="Inertie %",names.arg = paste("Axe",1: nrow(res$eig)),las=2,cex.axis=0.7,cex.lab=0.8)
